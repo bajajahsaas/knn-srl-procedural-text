48a82158414381ce164a89713a99b2f548316aaa
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-09 16:41:04 -0500

    Correct data split (60:20:20)

--- copy_model/split_data.py
+++ copy_model/split_data.py
@@ -5,7 +5,7 @@ with open(sys.argv[1], 'rb') as f:
     data = pickle.load(f)
 
 random.shuffle(data)
-trainsplit = int(0.8*len(data))
+trainsplit = int(0.75*len(data))
 with open('train.pkl', 'wb') as f:
     pickle.dump(data[:trainsplit], f)
 with open('val.pkl', 'wb') as f:
