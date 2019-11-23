459c1b55645c5f5bb286114f8dc87085dd144305
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-23 16:27:58 -0500

    [Experiment_train] training

--- copy_model/train_gen_exp_train.sh
+++ copy_model/train_gen_exp_train.sh
@@ -10,8 +10,8 @@
 #SBATCH --ntasks=1
 
 #python -u train.py --traindata train5.pkl --valdata val5.pkl --model_path models/generate5.pt --plot_path logsgen_exp_train/5 --no-copy --gpu
-python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/generate10.pt --plot_path logsgen_exp_train/10 --no-copy --gpu
-python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/generate20.pt --plot_path logsgen_exp_train/20 --no-copy --gpu
-#python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/generate50.pt --plot_path logsgen_exp_train/50 --no-copy --gpu
+#python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/generate10.pt --plot_path logsgen_exp_train/10 --no-copy --gpu
+#python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/generate20.pt --plot_path logsgen_exp_train/20 --no-copy --gpu
+python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/generate50.pt --plot_path logsgen_exp_train/50 --no-copy --gpu
 #sleep 1
 exit
