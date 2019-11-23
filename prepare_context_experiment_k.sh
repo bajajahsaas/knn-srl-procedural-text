cb808e7e8603389d360113b5756c43c034e9857d
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-23 13:28:59 -0500

    [Experiment_k] Minor Changes

--- prepare_context_experiment_k.sh
+++ prepare_context_experiment_k.sh
@@ -4,12 +4,12 @@
 #SBATCH --output=logsprepcontext_exp_k/copy_%j.txt  # output file
 #SBATCH -e logsprepcontext_exp_k/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-#SBATCH --mem=10000
+#SBATCH --mem=40G
 #
 #SBATCH --ntasks=1
 
 
-python -u copy_model/prepare_context.py replaced100.annoy replaced_tfidf100.pkl train_embeddings100.pkl train_embeddings100.pkl train100.pkl
+#python -u copy_model/prepare_context.py replaced100.annoy replaced_tfidf100.pkl train_embeddings100.pkl train_embeddings100.pkl train100.pkl
 python -u copy_model/prepare_context.py replaced100.annoy replaced_tfidf100.pkl train_embeddings100.pkl test_embeddings.pkl test100.pkl
 python -u copy_model/prepare_context.py replaced100.annoy replaced_tfidf100.pkl train_embeddings100.pkl val_embeddings.pkl val100.pkl
 
