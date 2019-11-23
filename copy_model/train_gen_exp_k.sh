13d6dc4f7dbb7f26a4c66cf22feedc1d1a3e1d58
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-23 18:02:14 -0500

    [Experiment_k] Run for k = 10

--- copy_model/train_gen_exp_k.sh
+++ copy_model/train_gen_exp_k.sh
@@ -11,6 +11,6 @@
 
 
 python -u train.py --traindata train100_K10.pkl --valdata val100_K10.pkl --model_path models/generate100_K10.pt --plot_path logsgen_exp_k/10 --no-copy --gpu
-python -u train.py --traindata train100_K20.pkl --valdata val100_K20.pkl --model_path models/generate100_K20.pt --plot_path logsgen_exp_k/20 --no-copy --gpu
+#python -u train.py --traindata train100_K20.pkl --valdata val100_K20.pkl --model_path models/generate100_K20.pt --plot_path logsgen_exp_k/20 --no-copy --gpu
 #sleep 1
 exit
