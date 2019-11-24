32987a17715f16cb2eb732c332cd9b86040d7a71
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-24 14:33:55 -0500

    [Experiment_k_train] test all bash script for K = 20

---
+++ copy_model/test_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=testing
+#SBATCH --output=logstest/test_%j.txt  # output file
+#SBATCH -e logstest/test_%j.err        # File to which STDERR will be written
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10000
+#
+#SBATCH --ntasks=1
+
+python -u test.py --generate --copy --valdata val100_K20.pkl --model_path models/copygen100_K20.pt --test_output_path copy_generate
+python -u test.py --no-generate --copy --valdata val100_K20.pkl --model_path models/copy100_K20.pt --test_output_path copy
+python -u test.py --generate --no-copy --valdata val100_K20.pkl --model_path models/generate100_K20.pt --test_output_path generate
+
+#sleep 1
+exit
