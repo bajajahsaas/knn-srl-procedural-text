b00453841b10643f12ed0220b4452ceaaec17466
Ahsaas Bajaj <bajajahsaas@gmail.com>
2019-11-24 14:41:49 -0500

    [Experiment_k_train] Bash file for complete testing

--- copy_model/test_all_exp_k.sh
+++ copy_model/test_all_exp_k.sh
@@ -8,6 +8,12 @@
 #
 #SBATCH --ntasks=1
 
+# Exper_k (K = 10)
+python -u test.py --generate --copy --valdata val100_K10.pkl --model_path models/copygen100_K10.pt --test_output_path copy_generate
+python -u test.py --no-generate --copy --valdata val100_K10.pkl --model_path models/copy100_K10.pt --test_output_path copy
+python -u test.py --generate --no-copy --valdata val100_K10.pkl --model_path models/generate100_K10.pt --test_output_path generate
+
+# Exper_k (K = 20)
 python -u test.py --generate --copy --valdata val100_K20.pkl --model_path models/copygen100_K20.pt --test_output_path copy_generate
 python -u test.py --no-generate --copy --valdata val100_K20.pkl --model_path models/copy100_K20.pt --test_output_path copy
 python -u test.py --generate --no-copy --valdata val100_K20.pkl --model_path models/generate100_K20.pt --test_output_path generate
