608f3e28beb2ece69241ce11a8b5445b3c264e90
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-01-30 16:33:17 -0500

    Add GPU partition in shell

--- copy_model/train_copy.sh
+++ copy_model/train_copy.sh
@@ -5,7 +5,7 @@
 #SBATCH -e logscopy/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
 #SBATCH --mem=100000
-#
+#SBATCH --partition=m40-long # Partition to submit to
 #SBATCH --ntasks=1
 
 python -u train.py --model_path models/copy.pt --plot_path logscopy --no-generate --gpu --classes 16
--- copy_model/train_gen.sh
+++ copy_model/train_gen.sh
@@ -5,7 +5,7 @@
 #SBATCH -e logsgen/generate_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
 #SBATCH --mem=100000
-#
+#SBATCH --partition=m40-long # Partition to submit to
 #SBATCH --ntasks=1
 
 python -u train.py --model_path models/generate.pt --plot_path logsgen --no-copy --gpu --classes 16
