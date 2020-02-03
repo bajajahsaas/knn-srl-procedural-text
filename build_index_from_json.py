a22228728c36b1d220b313a61ed5d7dcac43b95a
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-02 22:59:12 -0500

    Add support for BERT retriever

--- build_index_from_json.py
+++ build_index_from_json.py
@@ -1,3 +1,5 @@
+import sys
+
 import nltk
 import json
 import operator
@@ -6,9 +8,13 @@ import numpy as np
 import csv
 import re
 import pickle
+
+import torch
 from annoy import AnnoyIndex
 from sklearn.feature_extraction.text import TfidfVectorizer
+from torch import nn
 
+from biobert import getscibertmodel
 
 with open('wetlabs_train.json', 'r') as f:
     data = json.load(f)
@@ -30,34 +36,73 @@ with open('wetlabs_train.json', 'r') as f:
 def build_annoy_tfidf(sentences):
     print('Getting tfidf vectors')
     # Get TF IDF representation for each sentence
-    vectorizer = TfidfVectorizer(ngram_range = (1, 4), min_df = 2, max_df = 1.0)
+    vectorizer = TfidfVectorizer(ngram_range=(1, 4), min_df=2, max_df=1.0)
     vectorizer.fit(sentences)
     X = vectorizer.transform(sentences)
-    
+
     # build annoy index
     num_features = len(vectorizer.get_feature_names())
-    t = AnnoyIndex(num_features, "angular") # NN with cosine distance
+    t = AnnoyIndex(num_features, "angular")  # NN with cosine distance
     print('Inserting into annoy')
     for i, sent in enumerate(X):
         t.add_item(i, sent.toarray()[0])
-        if i%100 == 0:
-            print('%d/%d done'%(i, len(sentences)))
+        if i % 100 == 0:
+            print('%d/%d done' % (i, len(sentences)))
     print('Building annoy')
     t.build(10)
     return vectorizer, t
 
 
-print('Building annoy index for replaced sentences')
-# Representations of replaced sentences
-v_rep, ann_rep = build_annoy_tfidf([x['replaced'] for x in data])
-ann_rep.save('replaced.annoy')
-with open('replaced_tfidf.pkl', 'wb') as f:
-    pickle.dump(v_rep, f)
+def build_annoy_bert(sentences):
+    tokenizer, model = getscibertmodel()
+    print('Loaded scibert model')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
+    X = {}
+    for iter, sent in enumerate(sentences):
+        bert_tokens_sentence = tokenizer.encode(sent, add_special_tokens=True)
+        with torch.no_grad():
+            bert_embeddings = \
+                model(torch.tensor([bert_tokens_sentence]).to(device))[0].squeeze(0)
+            f_emb_avg = torch.mean(bert_embeddings, axis=0).cpu().numpy()
+            X[sent] = f_emb_avg
+
+        if iter % 1000 == 0:
+            print("BERT ready for ", iter, " sentences")
+
+    # build annoy index
+    num_features = 768
+    t = AnnoyIndex(num_features, "angular")  # NN with cosine distance
+    print('Inserting into annoy')
+    for i, sent in enumerate(X):
+        t.add_item(i, sent.toarray()[0])
+        if i % 100 == 0:
+            print('%d/%d done' % (i, len(sentences)))
+    print('Building annoy')
+    t.build(10)
+    return t
 
 
-print('Building annoy index for original documents')
-# Representations of original sentences
-v_ori, ann_ori = build_annoy_tfidf([x['sentence'] for x in data])
-ann_ori.save('original.annoy')
-with open('original_tfidf.pkl', 'wb') as f:
-    pickle.dump(v_ori, f)
+if sys.argv[1] == 'scibert':
+    print('Using BERT for retriever')
+    print('Building annoy index for original documents')
+    # Representations of original sentences
+    ann_ori = build_annoy_bert([x['sentence'] for x in data])
+    ann_ori.save('original.annoy')
+
+else:
+    print('Using tf-idf for retriever')
+    print('Building annoy index for replaced sentences')
+    # Representations of replaced sentences
+    v_rep, ann_rep = build_annoy_tfidf([x['replaced'] for x in data])
+    ann_rep.save('replaced.annoy')
+    with open('replaced_tfidf.pkl', 'wb') as f:
+        pickle.dump(v_rep, f)
+
+    print('Building annoy index for original documents')
+    # Representations of original sentences
+    v_ori, ann_ori = build_annoy_tfidf([x['sentence'] for x in data])
+    ann_ori.save('original.annoy')
+    with open('original_tfidf.pkl', 'wb') as f:
+        pickle.dump(v_ori, f)
--- copy_model/prepare_context.py
+++ copy_model/prepare_context.py
@@ -2,15 +2,19 @@ import sys
 import math
 import numpy as np
 import os
+
+import torch
 from annoy import AnnoyIndex
 import pickle
 import re
+
+from biobert import getscibertmodel
 from buckets import Buckets
 
 
 def get_relations_embeddings(s):
     # Get all relations including no relation
-    relset = set([(a,b) for a, b, _ in s['relations']])
+    relset = set([(a, b) for a, b, _ in s['relations']])
     norels = []
     if len(s['entities']) <= 1:
         return None, None, None, None, None, None, None, None
@@ -18,8 +22,8 @@ def get_relations_embeddings(s):
         for j in range(len(s['entities'])):
             if i == j or (i, j) in relset:
                 continue
-            norels.append((i,j,'No relation'))
-    all_rels = [(a,b,rel_dic[re.sub('\d+$','',c)]) for a,b,c in s['relations'] + norels]
+            norels.append((i, j, 'No relation'))
+    all_rels = [(a, b, rel_dic[re.sub('\d+$', '', c)]) for a, b, c in s['relations'] + norels]
     head = np.stack([s['entities'][x[0]][2] for x in all_rels])
     head_text = [s['entities'][x[0]][0] for x in all_rels]
     head_type = np.stack([ent_dic[s['entities'][x[0]][1]] for x in all_rels])
@@ -28,17 +32,16 @@ def get_relations_embeddings(s):
     tail_type = np.stack([ent_dic[s['entities'][x[1]][1]] for x in all_rels])
     labels = np.stack([x[2] for x in all_rels])
     posdiff = np.asarray([buckets.get_bucket(abs(s['entities'][t][3][0] -
-                                             s['entities'][h][3][0])) \
-                                for h,t,_ in all_rels])
+                                                 s['entities'][h][3][0])) \
+                          for h, t, _ in all_rels])
     return head_text, head, head_type, tail_text, tail, tail_type, labels, posdiff
 
 
-
-
 annoy_file = sys.argv[1]
-vectorizer_file = sys.argv[2]
+vectorizer_file = sys.argv[2]  # not used in case of bert retriever
 bert_data = sys.argv[3]
 bert_data_target = sys.argv[4]
+retriever = sys.argv[6]
 
 with open(bert_data, 'rb') as f:
     data_src = pickle.load(f)
@@ -46,15 +49,23 @@ with open(bert_data, 'rb') as f:
 with open(bert_data_target, 'rb') as f:
     data_tgt = pickle.load(f)
 
-with open(vectorizer_file, 'rb') as f:
-    vect = pickle.load(f)
+if retriever == 'tfidf':
+    with open(vectorizer_file, 'rb') as f:
+        vect = pickle.load(f)
+    t = AnnoyIndex(len(vect.get_feature_names()), 'angular')
 
-t = AnnoyIndex(len(vect.get_feature_names()), 'angular')
+else:
+    # vect file not required
+    t = t = AnnoyIndex(768, "angular")  # BERT: num_features = 768
+    tokenizer, model = getscibertmodel()
+    print('Loaded scibert model')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+
 t.load(annoy_file)
-K = 5 # 5 nearest neighbors
+K = 5  # 5 nearest neighbors
 
 bad_relations = set(['Misc-Link'])
-
 
 if os.path.exists('relations.txt') and os.path.exists('entity_types.txt'):
     with open('relations.txt', 'r') as f:
@@ -67,7 +78,7 @@ else:
     for s in data_src:
         for r in s['relations']:
             print(r)
-            relation_without_trailing = re.sub('\d+$','',r[2]) 
+            relation_without_trailing = re.sub('\d+$', '', r[2])
             relations.add(relation_without_trailing)
         for e in s['entities']:
             entity_types.add(e[1])
@@ -85,7 +96,7 @@ else:
     lengths = [s['length'] for s in data_src]
     sorted_lengths = sorted(lengths)
     # take 90th percentile to avoid outlier sentences
-    mx = sorted_lengths[int(len(sorted_lengths)*0.9)]
+    mx = sorted_lengths[int(len(sorted_lengths) * 0.9)]
     mn = 1
     buckets = Buckets(mn, mx, bucket_size=4)
     with open('buckets.pkl', 'wb') as f:
@@ -94,27 +105,40 @@ else:
 print(relations)
 print(len(relations))
 relations.append('No relation')
-rel_dic = {r:i for i, r in enumerate(relations)}
+rel_dic = {r: i for i, r in enumerate(relations)}
 for r in bad_relations:
-    rel_dic[r] = len(relations) -1 # norelation
+    rel_dic[r] = len(relations) - 1  # norelation
 print(entity_types)
 print(len(entity_types))
-ent_dic = {e:i for i, e in enumerate(entity_types)}
-
+ent_dic = {e: i for i, e in enumerate(entity_types)}
 
 dataset = []
 cnt = 0
+
+
+def getbertemb(sent):
+    bert_tokens_sentence = tokenizer.encode(sent, add_special_tokens=True)
+    with torch.no_grad():
+        bert_embeddings = \
+            model(torch.tensor([bert_tokens_sentence]).to(device))[0].squeeze(0)
+        f_emb_avg = torch.mean(bert_embeddings, axis=0).cpu().numpy()
+        return f_emb_avg
+
+
 for s in data_tgt:
-    print('%d / %d done'%(cnt, len(data_tgt)))
+    print('%d / %d done' % (cnt, len(data_tgt)))
     cnt += 1
-    vector = vect.transform([s['replaced']]).toarray()[0]
-    if bert_data == bert_data_target: # on training data, we are bound to find
-                                      # same sentence, so we exclude the top
-                                      # match
-        nns = t.get_nns_by_vector(vector, K+1)[1:] #1st one will be the same
-                                                # sentence
-    else: # on held out data, we needn't exclude the top match
-        nns = t.get_nns_by_vector(vector, K+1) 
+    if retriever == 'tfidf':
+        vector = vect.transform([s['replaced']]).toarray()[0]
+    else:
+        vector = getbertemb(s['replaced'])
+    if bert_data == bert_data_target:  # on training data, we are bound to find
+        # same sentence, so we exclude the top
+        # match
+        nns = t.get_nns_by_vector(vector, K + 1)[1:]  # 1st one will be the same
+        # sentence
+    else:  # on held out data, we needn't exclude the top match
+        nns = t.get_nns_by_vector(vector, K + 1)
 
     context_head = []
     context_head_text = []
@@ -138,43 +162,43 @@ for s in data_tgt:
             context_head_type.append(head_type)
             context_tail_type.append(tail_type)
             context_posdiffs.append(posdiff)
-    
+
     if num_context > 0:
         context_head = np.concatenate(context_head, axis=0)
         context_tail = np.concatenate(context_tail, axis=0)
-        context_label = np.concatenate(context_label, axis = 0)
-        context_head_type = np.concatenate(context_head_type, axis = 0)
-        context_tail_type = np.concatenate(context_tail_type, axis = 0)
-        context_posdiffs = np.concatenate(context_posdiffs, axis = 0)
+        context_label = np.concatenate(context_label, axis=0)
+        context_head_type = np.concatenate(context_head_type, axis=0)
+        context_tail_type = np.concatenate(context_tail_type, axis=0)
+        context_posdiffs = np.concatenate(context_posdiffs, axis=0)
     else:
         context_head, context_head_type, context_tail, context_tail_type, \
-                context_label, context_posdiffs = None, None, None, None, None, None
+        context_label, context_posdiffs = None, None, None, None, None, None
 
-    query_head_text, query_head, query_head_type, query_tail_text, query_tail,\
-        query_tail_type, query_labels, query_posdiff = get_relations_embeddings(s)
+    query_head_text, query_head, query_head_type, query_tail_text, query_tail, \
+    query_tail_type, query_labels, query_posdiff = get_relations_embeddings(s)
     if query_head is None:
         continue
     dataset.append({
-                        'query_sent' : s['sentence'],
-                        'context_sents' : [data_src[x]['sentence'] for x in \
-                                           nns],
-                        'query_head': query_head,
-                        'query_head_text' : query_head_text,
-                        'query_head_type': query_head_type,
-                        'query_tail': query_tail,
-                        'query_tail_text' : query_tail_text,
-                        'query_tail_type': query_tail_type,
-                        'query_posdiff': query_posdiff,
-                        'query_labels': query_labels,
-                        'context_head' : context_head,
-                        'context_head_text' : context_head_text,
-                        'context_head_type': context_head_type,
-                        'context_tail' : context_tail,
-                        'context_tail_text' : context_tail_text,
-                        'context_tail_type': context_tail_type,
-                        'context_labels' : context_label,
-                        'context_posdiff' : context_posdiffs
-                    })
+        'query_sent': s['sentence'],
+        'context_sents': [data_src[x]['sentence'] for x in \
+                          nns],
+        'query_head': query_head,
+        'query_head_text': query_head_text,
+        'query_head_type': query_head_type,
+        'query_tail': query_tail,
+        'query_tail_text': query_tail_text,
+        'query_tail_type': query_tail_type,
+        'query_posdiff': query_posdiff,
+        'query_labels': query_labels,
+        'context_head': context_head,
+        'context_head_text': context_head_text,
+        'context_head_type': context_head_type,
+        'context_tail': context_tail,
+        'context_tail_text': context_tail_text,
+        'context_tail_type': context_tail_type,
+        'context_labels': context_label,
+        'context_posdiff': context_posdiffs
+    })
 
 with open(sys.argv[5], 'wb') as f:
     pickle.dump(dataset, f)
