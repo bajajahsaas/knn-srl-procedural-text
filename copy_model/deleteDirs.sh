331634f720e3ef3c78a9accb4cce3e983b73c98d
Ahsaas Bajaj <abajaj@node175.cm.cluster>
2020-02-03 01:33:40 -0500

    script for del dirs

---
+++ copy_model/deleteDirs.sh
@@ -0,0 +1,23 @@
+cd models
+rm *
+cd ..
+cd copy
+rm *
+cd ..
+cd generate 
+rm *
+cd ..
+cd copy_generate
+rm *
+cd ..
+cd logscopy
+rm *
+cd ..
+cd logsgen
+rm *
+cd ..
+cd logscopygen
+rm *
+cd ..
+
+
