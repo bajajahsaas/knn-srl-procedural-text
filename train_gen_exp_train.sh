9a06bd9f97b9f93db490c614dfdd332eff6dccac
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-08 20:57:52 -0500

    [Experiment train] Set args, run for 5

--- train_gen_exp_train.sh
+++ train_gen_exp_train.sh
@@ -9,10 +9,10 @@
 #
 #SBATCH --ntasks=1
 
-python -u train.py --traindata train5.pkl --valdata val5.pkl --model_path models/generate5.pt --plot_path logsgen_exp_train/5 --no-copy --gpu
-python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/generate10.pt --plot_path logsgen_exp_train/10 --no-copy --gpu
-python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/generate20.pt --plot_path logsgen_exp_train/20 --no-copy --gpu
-python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/generate50.pt --plot_path logsgen_exp_train/50 --no-copy --gpu
-python -u train.py --traindata train100.pkl --valdata val100.pkl --model_path models/generate100.pt --plot_path logsgen_exp_train/100 --no-copy --gpu
+python -u train.py --traindata train5.pkl --valdata val5.pkl --model_path models/generate5.pt --plot_path logsgen_exp_train/5 --no-copy --generate --gpu
+#python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/generate10.pt --plot_path logsgen_exp_train/10 --no-copy --generate --gpu
+#python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/generate20.pt --plot_path logsgen_exp_train/20 --no-copy --generate --gpu
+#python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/generate50.pt --plot_path logsgen_exp_train/50 --no-copy --generate --gpu
+#python -u train.py --traindata train100.pkl --valdata val100.pkl --model_path models/generate100.pt --plot_path logsgen_exp_train/100 --no-copy --generate --gpu
 #sleep 1
 exit
