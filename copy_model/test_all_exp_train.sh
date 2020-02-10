a812f18b66ae8bc58bafa8a211a0cad0976164b5
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-09 21:51:35 -0500

    test on 5%

--- copy_model/test_all_exp_train.sh
+++ copy_model/test_all_exp_train.sh
@@ -13,26 +13,26 @@
 #python -u test.py --no-generate --copy --valdata val5.pkl --model_path models/copy5.pt --test_output_path copy
 #python -u test.py --generate --no-copy --valdata val5.pkl --model_path models/generate5.pt --test_output_path generate
 
-# Exper_train (percen = 10)
-python -u test.py --generate --copy --valdata val10.pkl --model_path models/copygen10.pt --test_output_path copy_generate
-python -u test.py --no-generate --copy --valdata val10.pkl --model_path models/copy10.pt --test_output_path copy
-python -u test.py --generate --no-copy --valdata val10.pkl --model_path models/generate10.pt --test_output_path generate
-
-
-# Exper_train (percen = 20)
-python -u test.py --generate --copy --valdata val20.pkl --model_path models/copygen20.pt --test_output_path copy_generate
-python -u test.py --no-generate --copy --valdata val20.pkl --model_path models/copy20.pt --test_output_path copy
-python -u test.py --generate --no-copy --valdata val20.pkl --model_path models/generate20.pt --test_output_path generate
-
-# Exper_train (percen = 50)
-python -u test.py --generate --copy --valdata val50.pkl --model_path models/copygen50.pt --test_output_path copy_generate
-python -u test.py --no-generate --copy --valdata val50.pkl --model_path models/copy50.pt --test_output_path copy
-python -u test.py --generate --no-copy --valdata val50.pkl --model_path models/generate50.pt --test_output_path generate
-
-# Exper_train (percen = 100)
-python -u test.py --generate --copy --valdata val100.pkl --model_path models/copygen100.pt --test_output_path copy_generate
-python -u test.py --no-generate --copy --valdata val100.pkl --model_path models/copy100.pt --test_output_path copy
-python -u test.py --generate --no-copy --valdata val100.pkl --model_path models/generate100.pt --test_output_path generate
+## Exper_train (percen = 10)
+#python -u test.py --generate --copy --valdata val10.pkl --model_path models/copygen10.pt --test_output_path copy_generate
+#python -u test.py --no-generate --copy --valdata val10.pkl --model_path models/copy10.pt --test_output_path copy
+#python -u test.py --generate --no-copy --valdata val10.pkl --model_path models/generate10.pt --test_output_path generate
+#
+#
+## Exper_train (percen = 20)
+#python -u test.py --generate --copy --valdata val20.pkl --model_path models/copygen20.pt --test_output_path copy_generate
+#python -u test.py --no-generate --copy --valdata val20.pkl --model_path models/copy20.pt --test_output_path copy
+#python -u test.py --generate --no-copy --valdata val20.pkl --model_path models/generate20.pt --test_output_path generate
+#
+## Exper_train (percen = 50)
+#python -u test.py --generate --copy --valdata val50.pkl --model_path models/copygen50.pt --test_output_path copy_generate
+#python -u test.py --no-generate --copy --valdata val50.pkl --model_path models/copy50.pt --test_output_path copy
+#python -u test.py --generate --no-copy --valdata val50.pkl --model_path models/generate50.pt --test_output_path generate
+#
+## Exper_train (percen = 100)
+#python -u test.py --generate --copy --valdata val100.pkl --model_path models/copygen100.pt --test_output_path copy_generate
+#python -u test.py --no-generate --copy --valdata val100.pkl --model_path models/copy100.pt --test_output_path copy
+#python -u test.py --generate --no-copy --valdata val100.pkl --model_path models/generate100.pt --test_output_path generate
 
 
 
@@ -42,25 +42,25 @@ python -u test.py --generate --copy --valdata test5.pkl --model_path models/copy
 python -u test.py --no-generate --copy --valdata test5.pkl --model_path models/copy5.pt --test_output_path copy_test
 python -u test.py --generate --no-copy --valdata test5.pkl --model_path models/generate5.pt --test_output_path generate_test
 
-# Exper_train (percen = 10)
-python -u test.py --generate --copy --valdata test10.pkl --model_path models/copygen10.pt --test_output_path copy_generate_test
-python -u test.py --no-generate --copy --valdata test10.pkl --model_path models/copy10.pt --test_output_path copy_test
-python -u test.py --generate --no-copy --valdata test10.pkl --model_path models/generate10.pt --test_output_path generate_test
-
-
-# Exper_train (percen = 20)
-python -u test.py --generate --copy --valdata test20.pkl --model_path models/copygen20.pt --test_output_path copy_generate_test
-python -u test.py --no-generate --copy --valdata test20.pkl --model_path models/copy20.pt --test_output_path copy_test
-python -u test.py --generate --no-copy --valdata test20.pkl --model_path models/generate20.pt --test_output_path generate_test
-
-# Exper_train (percen = 50)
-python -u test.py --generate --copy --valdata test50.pkl --model_path models/copygen50.pt --test_output_path copy_generate_test
-python -u test.py --no-generate --copy --valdata test50.pkl --model_path models/copy50.pt --test_output_path copy_test
-python -u test.py --generate --no-copy --valdata test50.pkl --model_path models/generate50.pt --test_output_path generate_test
-
-# Exper_train (percen = 100)
-python -u test.py --generate --copy --valdata test100.pkl --model_path models/copygen100.pt --test_output_path copy_generate_test
-python -u test.py --no-generate --copy --valdata test100.pkl --model_path models/copy100.pt --test_output_path copy_test
-python -u test.py --generate --no-copy --valdata test100.pkl --model_path models/generate100.pt --test_output_path generate_test
+## Exper_train (percen = 10)
+#python -u test.py --generate --copy --valdata test10.pkl --model_path models/copygen10.pt --test_output_path copy_generate_test
+#python -u test.py --no-generate --copy --valdata test10.pkl --model_path models/copy10.pt --test_output_path copy_test
+#python -u test.py --generate --no-copy --valdata test10.pkl --model_path models/generate10.pt --test_output_path generate_test
+#
+#
+## Exper_train (percen = 20)
+#python -u test.py --generate --copy --valdata test20.pkl --model_path models/copygen20.pt --test_output_path copy_generate_test
+#python -u test.py --no-generate --copy --valdata test20.pkl --model_path models/copy20.pt --test_output_path copy_test
+#python -u test.py --generate --no-copy --valdata test20.pkl --model_path models/generate20.pt --test_output_path generate_test
+#
+## Exper_train (percen = 50)
+#python -u test.py --generate --copy --valdata test50.pkl --model_path models/copygen50.pt --test_output_path copy_generate_test
+#python -u test.py --no-generate --copy --valdata test50.pkl --model_path models/copy50.pt --test_output_path copy_test
+#python -u test.py --generate --no-copy --valdata test50.pkl --model_path models/generate50.pt --test_output_path generate_test
+#
+## Exper_train (percen = 100)
+#python -u test.py --generate --copy --valdata test100.pkl --model_path models/copygen100.pt --test_output_path copy_generate_test
+#python -u test.py --no-generate --copy --valdata test100.pkl --model_path models/copy100.pt --test_output_path copy_test
+#python -u test.py --generate --no-copy --valdata test100.pkl --model_path models/generate100.pt --test_output_path generate_test
 #sleep 1
 exit
