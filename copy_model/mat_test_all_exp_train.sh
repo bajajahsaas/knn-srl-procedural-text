e62ba098f9d4319bee5c1862b66d1ace9240d239
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-10 13:55:50 -0500

    Revert "MatSci reduce layer sizes"
    This reverts commit 0fd6c187

--- copy_model/mat_test_all_exp_train.sh
+++ copy_model/mat_test_all_exp_train.sh
@@ -9,56 +9,58 @@
 #SBATCH --ntasks=1
 
 # Exper_train (percen = 5)
-python -u test.py --generate --copy --valdata val5.pkl --model_path models/copygen5.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata val5.pkl --model_path models/copy5.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata val5.pkl --model_path models/copygen5.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata val5.pkl --model_path models/copy5.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata val5.pkl --model_path models/generate5.pt --test_output_path generate --classes 16 --num_entities 21 --num_buckets 11
 
 # Exper_train (percen = 10)
-python -u test.py --generate --copy --valdata val10.pkl --model_path models/copygen10.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata val10.pkl --model_path models/copy10.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata val10.pkl --model_path models/copygen10.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata val10.pkl --model_path models/copy10.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata val10.pkl --model_path models/generate10.pt --test_output_path generate --classes 16 --num_entities 21 --num_buckets 11
 
 
 # Exper_train (percen = 20)
-python -u test.py --generate --copy --valdata val20.pkl --model_path models/copygen20.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata val20.pkl --model_path models/copy20.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata val20.pkl --model_path models/copygen20.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata val20.pkl --model_path models/copy20.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata val20.pkl --model_path models/generate20.pt --test_output_path generate --classes 16 --num_entities 21 --num_buckets 11
 
 # Exper_train (percen = 50)
-python -u test.py --generate --copy --valdata val50.pkl --model_path models/copygen50.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata val50.pkl --model_path models/copy50.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata val50.pkl --model_path models/copygen50.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata val50.pkl --model_path models/copy50.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata val50.pkl --model_path models/generate50.pt --test_output_path generate --classes 16 --num_entities 21 --num_buckets 11
 
 # Exper_train (percen = 100)
-python -u test.py --generate --copy --valdata val100.pkl --model_path models/copygen100.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata val100.pkl --model_path models/copy100.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata val100.pkl --model_path models/copygen100.pt --test_output_path copy_generate --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata val100.pkl --model_path models/copy100.pt --test_output_path copy --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata val100.pkl --model_path models/generate100.pt --test_output_path generate --classes 16 --num_entities 21 --num_buckets 11
 
 
 
 ## on Test split
 # Exper_train (percen = 5)
-python -u test.py --generate --copy --valdata test5.pkl --model_path models/copygen5.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata test5.pkl --model_path models/copy5.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata test5.pkl --model_path models/copygen5.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata test5.pkl --model_path models/copy5.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata test5.pkl --model_path models/generate5.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+
 # Exper_train (percen = 10)
-python -u test.py --generate --copy --valdata test10.pkl --model_path models/copygen10.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata test10.pkl --model_path models/copy10.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata test10.pkl --model_path models/copygen10.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata test10.pkl --model_path models/copy10.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata test10.pkl --model_path models/generate10.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
 
+
 # Exper_train (percen = 20)
-python -u test.py --generate --copy --valdata test20.pkl --model_path models/copygen20.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata test20.pkl --model_path models/copy20.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata test20.pkl --model_path models/copygen20.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata test20.pkl --model_path models/copy20.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata test20.pkl --model_path models/generate20.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
 
 # Exper_train (percen = 50)
-python -u test.py --generate --copy --valdata test50.pkl --model_path models/copygen50.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata test50.pkl --model_path models/copy50.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata test50.pkl --model_path models/copygen50.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata test50.pkl --model_path models/copy50.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata test50.pkl --model_path models/generate50.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
 
 # Exper_train (percen = 100)
-python -u test.py --generate --copy --valdata test100.pkl --model_path models/copygen100.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u test.py --no-generate --copy --valdata test100.pkl --model_path models/copy100.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u test.py --generate --copy --valdata test100.pkl --model_path models/copygen100.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
+python -u test.py --no-generate --copy --valdata test100.pkl --model_path models/copy100.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
 python -u test.py --generate --no-copy --valdata test100.pkl --model_path models/generate100.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
 #sleep 1
 exit
--- copy_model/mat_train_copy_gen_exp_train.sh
+++ copy_model/mat_train_copy_gen_exp_train.sh
@@ -5,14 +5,14 @@
 #SBATCH -e logscopygen_exp_train/copy_gen_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m40-long # Partition to submit to
-#SBATCH --mem=50G
+#SBATCH --mem=180G
 #
 #SBATCH --ntasks=1
 
-python -u train.py --traindata train5.pkl --valdata val5.pkl --model_path models/copygen5.pt --plot_path logscopygen_exp_train/5 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 8 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/copygen10.pt --plot_path logscopygen_exp_train/10 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 8 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/copygen20.pt --plot_path logscopygen_exp_train/20 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 8 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/copygen50.pt --plot_path logscopygen_exp_train/50 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 8 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
-python -u train.py --traindata train100.pkl --valdata val100.pkl --model_path models/copygen100.pt --plot_path logscopygen_exp_train/100 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 8 --context_label_dim 128 --relation_output_dim 128 --attention_hidden_dims [256,128] --relation_hidden_dims [256,128] --shouldcopy_hidden_dims [256,128,64]
+python -u train.py --traindata train5.pkl --valdata val5.pkl --model_path models/copygen5.pt --plot_path logscopygen_exp_train/5 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/copygen10.pt --plot_path logscopygen_exp_train/10 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/copygen20.pt --plot_path logscopygen_exp_train/20 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/copygen50.pt --plot_path logscopygen_exp_train/50 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata train100.pkl --valdata val100.pkl --model_path models/copygen100.pt --plot_path logscopygen_exp_train/100 --generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
 #sleep 1
 exit
