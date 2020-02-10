9667b7508be9b7829ae619727eef73d44e98d8fe
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-09 22:57:40 -0500

    Shell for delete dirs

---
+++ copy_model/deletedirs.sh
@@ -0,0 +1,42 @@
+cd logstest
+rm *
+cd ..
+cd models
+rm *
+cd ..
+cd copy
+rm *
+cd ..
+cd generate
+rm *
+cd ..
+cd copy_generate
+rm *
+cd ..
+cd logscopy
+rm *
+cd ..
+cd logsgen
+rm *
+cd ..
+cd logscopygen
+rm *
+cd ..
+cd copy_test
+rm *
+cd ..
+cd generate_test
+rm *
+cd ..
+cd copy_generate_test
+rm *
+cd ..
+cd logscopygen_exp_train
+rm * -r
+cd ..
+cd logscopy_exp_train
+rm * -r
+cd ..
+cd logsgen_exp_train
+rm * -r
+cd ..
