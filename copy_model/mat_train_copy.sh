a2d30705145a1b739cc414bc20dd27ced86827c9
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-09 23:30:47 -0500

    Shell for materials data training

---
+++ copy_model/mat_train_copy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=copy
+#SBATCH --output=logscopy/copy_%j.txt  # output file
+#SBATCH -e logscopy/copy_%j.err        # File to which STDERR will be written
+#SBATCH --gres=gpu:1
+#SBATCH --partition=m40-long # Partition to submit to
+#SBATCH --mem=10000
+#
+#SBATCH --ntasks=1
+
+python -u train.py --model_path models/copy.pt --plot_path logscopy --no-generate --gpu --classes 16 --num_entities 21 --num_buckets 11
+#sleep 1
+exit
