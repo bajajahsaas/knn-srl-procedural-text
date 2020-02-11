2ef1e9386256fdfd4cd98091e9e4d690ef084fff
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-02-11 13:15:58 -0500

    change gpu

--- copy_model/mat_train_copy_gen_exp_train.sh
+++ copy_model/mat_train_copy_gen_exp_train.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=logscopygen_exp_train/copy_gen_%j.txt  # output file
 #SBATCH -e logscopygen_exp_train/copy_gen_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-#SBATCH --partition=m40-long # Partition to submit to
+#SBATCH --partition=1080ti-long # Partition to submit to
 #SBATCH --mem=180G
 #
 #SBATCH --ntasks=1
