cc510da129c4a2c14edd84e46cadbd1344957bb1
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-06 03:45:26 -0500

    [exp_k] Test for higher k

--- prepare_data_k.sh
+++ prepare_data_k.sh
@@ -1,7 +1,5 @@
 
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk1.pkl scibert 8 1
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk2.pkl scibert 8 2
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk5.pkl scibert 8 5
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk10.pkl scibert 8 10
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk15.pkl scibert 8 15
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk20.pkl scibert 8 20
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk20.pkl scibert 4 20
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk30.pkl scibert 4 30
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk40.pkl scibert 4 40
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk50.pkl scibert 4 50
