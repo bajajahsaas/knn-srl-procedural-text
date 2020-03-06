e2a1b060ba5837c7115da6d4f49b795d985605a7
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-06 16:09:40 -0500

    [exp-k] Prepare data

---
+++ copy_model/test_all_nn.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+#SBATCH --job-name=testing
+#SBATCH --output=logstest/test_%j.txt  # output file
+#SBATCH -e logstest/test_%j.err        # File to which STDERR will be written
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#
+#SBATCH --ntasks=1
+
+## on Test split
+# Exper_train (K = 1)
+#python -u test_k.py --generate --copy --valdata testk1.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+#python -u test_k.py --no-generate --copy --valdata testk1.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk1.pkl --model_path models/generate.pt --test_output_path generate_test
+#
+## Exper_train (K = 2)
+#python -u test_k.py --generate --copy --valdata testk2.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+#python -u test_k.py --no-generate --copy --valdata testk2.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk2.pkl --model_path models/generate.pt --test_output_path generate_test
+#
+## Exper_train (K = 5)
+#python -u test_k.py --generate --copy --valdata testk5.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+#python -u test_k.py --no-generate --copy --valdata testk5.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk5.pkl --model_path models/generate.pt --test_output_path generate_test
+#
+## Exper_train (K = 10)
+#python -u test_k.py --generate --copy --valdata testk10.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+#python -u test_k.py --no-generate --copy --valdata testk10.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk10.pkl --model_path models/generate.pt --test_output_path generate_test
+#
+## Exper_train (K = 15)
+#python -u test_k.py --generate --copy --valdata testk15.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+#python -u test_k.py --no-generate --copy --valdata testk15.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk15.pkl --model_path models/generate.pt --test_output_path generate_test
+
+# Exper_train (K = 20)
+#python -u test_k.py --generate --copy --valdata testk20.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+python -u test_k.py --no-generate --copy --valdata testk20.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk20.pkl --model_path models/generate.pt --test_output_path generate_test
+
+# Exper_train (K = 30)
+#python -u test_k.py --generate --copy --valdata testk30.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+python -u test_k.py --no-generate --copy --valdata testk30.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk30.pkl --model_path models/generate.pt --test_output_path generate_test
+
+# Exper_train (K = 40)
+#python -u test_k.py --generate --copy --valdata testk40.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+python -u test_k.py --no-generate --copy --valdata testk40.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk40.pkl --model_path models/generate.pt --test_output_path generate_test
+
+# Exper_train (K = 50)
+#python -u test_k.py --generate --copy --valdata testk50.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
+python -u test_k.py --no-generate --copy --valdata testk50.pkl --model_path models/copy.pt --test_output_path copy_test
+#python -u test_k.py --generate --no-copy --valdata testk50.pkl --model_path models/generate.pt --test_output_path generate_test
--- prepare_data_k.sh
+++ prepare_data_k.sh
@@ -1,5 +1,8 @@
-
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk20.pkl scibert 4 20
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk30.pkl scibert 4 30
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk40.pkl scibert 4 40
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk50.pkl scibert 4 50
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk1.pkl scibert 4 1
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk2.pkl scibert 4 2
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk5.pkl scibert 4 5
+python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk10.pkl scibert 4 10
+#python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk20.pkl scibert 4 20
+#python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk30.pkl scibert 4 30
+#python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk40.pkl scibert 4 40
+#python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl testk50.pkl scibert 4 50
