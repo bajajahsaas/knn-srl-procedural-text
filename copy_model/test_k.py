63fee0b5176ef2420c5b8e6e53ccddb71854c9de
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-06 04:33:03 -0500

    Revert test_k without copyprob

--- copy_model/test_k.py
+++ copy_model/test_k.py
@@ -106,10 +106,9 @@ def accuracy(data, model):
         f = open(os.path.join(args.test_output_path, "predictions_" + args.valdata + '.csv'), 'w')
         writer = csv.writer(f)
         writer.writerow(['Sentence', 'Relations in context', 'Head', 'Tail',
-                         'Target', 'Prediction', 'correct', 'copyprob'])
+                         'Target', 'Prediction', 'correct'])
 
-        def write_csv_row(writer, sent, qtext, qlabels, ctext, clabels, pred, copyprob):
-            copyprob = torch.exp(copyprob.view(-1))
+        def write_csv_row(writer, sent, qtext, qlabels, ctext, clabels, pred):
             qlabels = qlabels.view(-1)
             if clabels is not None:
                 clabels = clabels.view(-1)
@@ -122,17 +121,16 @@ def accuracy(data, model):
                 copyable = '|'.join(list(set([relations[x] for x in clabels])))
             else:
                 copyable = 'No Context found'
-            for a,b,c,d,e in zip(qh,qt,qlabels, pred, copyprob):
+            for a,b,c,d in zip(qh,qt,qlabels, pred):
                 writer.writerow([sent, copyable, a, b, relations[c],
-                                 relations[d], c==d, '%.5f'%e])
+                                 relations[d], c==d])
 
 
 
 
 
         for sent, q_text, cxt_text, q, cxt, cxt_labels, q_labels, mask in get_batches(data):
-            logprob, copyprob = model(q, cxt, cxt_labels, mask)
-            pred = torch.argmax(logprob, \
+            pred = torch.argmax(model(q, cxt, cxt_labels, mask)[0], \
                                 dim=-1).view(-1)
 
             this_target = q_labels.view(-1).data.detach().numpy().copy()
@@ -144,7 +142,7 @@ def accuracy(data, model):
 
             all_target.append(this_target)
             all_pred.append(this_pred)
-            write_csv_row(writer,sent, q_text, q_labels, cxt_text, cxt_labels, pred, copyprob)
+            write_csv_row(writer,sent, q_text, q_labels, cxt_text, cxt_labels, pred)
         f.close()
         all_pred = np.concatenate(all_pred, 0)
         all_target = np.concatenate(all_target, 0)
