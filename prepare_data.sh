9e4b91c1e48be76431c1e6f28878d5b3ae1fb228
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-06 04:54:41 -0500

    Change sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -8,6 +8,6 @@ python copy_model/prepare_context.py original.annoy original_bert.pkl train_embe
 python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl test_NN_10.pkl scibert 4 10
 python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl val_NN_10.pkl scibert 4 10
 
-python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl train_embeddings.pkl train_NN_10.pkl scibert 4 20
-python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl test_NN_10.pkl scibert 4 20
-python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl val_NN_10.pkl scibert 4 20
+#python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl train_embeddings.pkl train_NN_20.pkl scibert 4 20
+#python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl test_embeddings.pkl test_NN_20.pkl scibert 4 20
+#python copy_model/prepare_context.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl val_NN_20.pkl scibert 4 20
