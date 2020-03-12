ef6575c51fc7164eaed6eb6c451cf71cbfdca4d2
Ahsaas Bajaj <bajajahsaas@gmail.com>
2020-03-12 03:47:27 -0400

    Run copy for all exp_k

--- copy_model/test_all_nn.sh
+++ copy_model/test_all_nn.sh
@@ -10,46 +10,57 @@
 
 ## on Test split
 # Exper_train (K = 1)
-#python -u test_k.py --generate --copy --valdata testk1.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk1.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk1.pkl --model_path models/generate.pt --test_output_path generate_test
 #
 ## Exper_train (K = 2)
-#python -u test_k.py --generate --copy --valdata testk2.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk2.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk2.pkl --model_path models/generate.pt --test_output_path generate_test
 #
 ## Exper_train (K = 5)
-#python -u test_k.py --generate --copy --valdata testk5.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk5.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk5.pkl --model_path models/generate.pt --test_output_path generate_test
 #
 ## Exper_train (K = 10)
-#python -u test_k.py --generate --copy --valdata testk10.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk10.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk10.pkl --model_path models/generate.pt --test_output_path generate_test
 #
 ## Exper_train (K = 15)
-#python -u test_k.py --generate --copy --valdata testk15.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk15.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk15.pkl --model_path models/generate.pt --test_output_path generate_test
 
 # Exper_train (K = 20)
-#python -u test_k.py --generate --copy --valdata testk20.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk20.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk20.pkl --model_path models/generate.pt --test_output_path generate_test
 
 # Exper_train (K = 30)
-#python -u test_k.py --generate --copy --valdata testk30.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk30.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk30.pkl --model_path models/generate.pt --test_output_path generate_test
 
 # Exper_train (K = 40)
-#python -u test_k.py --generate --copy --valdata testk40.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk40.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk40.pkl --model_path models/generate.pt --test_output_path generate_test
 
 # Exper_train (K = 50)
-#python -u test_k.py --generate --copy --valdata testk50.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test
 python -u test_k.py --no-generate --copy --valdata testk50.pkl --model_path models/copy_nn10.pt --test_output_path copy_test
-#python -u test_k.py --generate --no-copy --valdata testk50.pkl --model_path models/generate.pt --test_output_path generate_test
+
+#####################
+
+# Exper_train (K = 1)
+python -u test_k.py --no-generate --copy --valdata testk1.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+#
+## Exper_train (K = 2)
+python -u test_k.py --no-generate --copy --valdata testk2.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+#
+## Exper_train (K = 5)
+python -u test_k.py --no-generate --copy --valdata testk5.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+#
+## Exper_train (K = 10)
+python -u test_k.py --no-generate --copy --valdata testk10.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+#
+## Exper_train (K = 15)
+python -u test_k.py --no-generate --copy --valdata testk15.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+
+# Exper_train (K = 20)
+python -u test_k.py --no-generate --copy --valdata testk20.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+
+# Exper_train (K = 30)
+python -u test_k.py --no-generate --copy --valdata testk30.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+
+# Exper_train (K = 40)
+python -u test_k.py --no-generate --copy --valdata testk40.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
+
+# Exper_train (K = 50)
+python -u test_k.py --no-generate --copy --valdata testk50.pkl --model_path models/copy_nn20.pt --test_output_path copy_test
