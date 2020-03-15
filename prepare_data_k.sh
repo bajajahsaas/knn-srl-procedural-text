2970d6210a333069de06585af794ff1478b4d854
Ahsaas Bajaj <abajaj@gypsum.cm.cluster>
2020-03-15 03:38:26 -0400

    Delete data dir

--- prepare_data_k.sh
+++ prepare_data_k.sh
@@ -4,8 +4,8 @@
 #SBATCH --output=logsprepdata/test_%j.txt  # output file
 #SBATCH -e logsprepdata/test_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-SBATCH --partition=m40-long # Partition to submit to
-#SBATCH --mem=40GB
+SBATCH --partition=m40-short # Partition to submit to
+#SBATCH --mem=50GB
 #
 #SBATCH --ntasks=1
 
@@ -25,6 +25,6 @@ SBATCH --partition=m40-long # Partition to submit to
 # python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk10.pkl scibert 4 10
 # python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk15.pkl scibert 4 15
 # python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk20.pkl scibert 4 20
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk30.pkl scibert 4 30
+# python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk30.pkl scibert 4 30
 python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk40.pkl scibert 4 40
-python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk50.pkl scibert 4 50
+# python copy_model/prepare_context_k.py original.annoy original_bert.pkl train_embeddings.pkl val_embeddings.pkl valk50.pkl scibert 4 50
