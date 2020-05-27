e6113d6ddb96c4806e1bfa57273d97af12c53d1e
Ahsaas Bajaj <abajaj@gypsum.cm.cluster>
2020-05-27 04:08:04 -0400

    update matsci train/test sh

--- copy_model/mat_test_all_k.sh
+++ copy_model/mat_test_all_k.sh
@@ -10,46 +10,94 @@
 
 ## on Test split
 # Exper_train (K = 1)
-#python -u test_k.py --generate --copy --valdata testk1.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk1.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk1.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk1.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk1.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk1.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 2)
-#python -u test_k.py --generate --copy --valdata testk2.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk2.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk2.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk2.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk2.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk2.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 5)
-#python -u test_k.py --generate --copy --valdata testk5.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk5.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk5.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk5.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk5.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk5.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 10)
-#python -u test_k.py --generate --copy --valdata testk10.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk10.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk10.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk10.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk10.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk10.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 15)
-#python -u test_k.py --generate --copy --valdata testk15.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk15.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk15.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk15.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk15.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk15.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 20)
-#python -u test_k.py --generate --copy --valdata testk20.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk20.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk20.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk20.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk20.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk20.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 30)
-#python -u test_k.py --generate --copy --valdata testk30.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk30.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk30.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk30.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk30.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk30.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 40)
-#python -u test_k.py --generate --copy --valdata testk40.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk40.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk40.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk40.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk40.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk40.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
 
 # Exper_train (K = 50)
-#python -u test_k.py --generate --copy --valdata testk50.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11
-python -u test_k.py --no-generate --copy --valdata testk50.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11
-#python -u test_k.py --generate --no-copy --valdata testk50.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk50.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk50.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/testk50.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+
+
+## on Val split
+# Exper_train (K = 1)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk1.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk1.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk1.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 2)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk2.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk2.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk2.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 5)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk5.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk5.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk5.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 10)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk10.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk10.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk10.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 15)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk15.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk15.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk15.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 20)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk20.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk20.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk20.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 30)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk30.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk30.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk30.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 40)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk40.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk40.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk40.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+
+# Exper_train (K = 50)
+#python -u test_k.py --generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk50.pkl --model_path models/copy_generate.pt --test_output_path copy_generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+python -u test_k.py --no-generate --copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk50.pkl --model_path models/copy.pt --test_output_path copy_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
+#python -u test_k.py --generate --no-copy --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr/exp_k/valk50.pkl --model_path models/generate.pt --test_output_path generate_test --classes 16 --num_entities 21 --num_buckets 11 --gpu
--- copy_model/mat_train_copy_exp_train.sh
+++ copy_model/mat_train_copy_exp_train.sh
@@ -4,17 +4,17 @@
 #SBATCH --output=logscopy_exp_train/copy_%j.txt  # output file
 #SBATCH -e logscopy_exp_train/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-#SBATCH --partition=m40-long # Partition to submit to
-#SBATCH --mem=180G
+#SBATCH --partition=2080ti-long # Partition to submit to
+#SBATCH --mem=40G
 #
 #SBATCH --ntasks=1
 
-python -u train.py --traindata train1.pkl --valdata val1.pkl --model_path models/copy1.pt --plot_path logscopy_exp_train/1 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
-python -u train.py --traindata train2.pkl --valdata val2.pkl --model_path models/copy2.pt --plot_path logscopy_exp_train/2 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
-python -u train.py --traindata train5.pkl --valdata val5.pkl --model_path models/copy5.pt --plot_path logscopy_exp_train/5 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
-python -u train.py --traindata train10.pkl --valdata val10.pkl --model_path models/copy10.pt --plot_path logscopy_exp_train/10 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
-#python -u train.py --traindata train20.pkl --valdata val20.pkl --model_path models/copy20.pt --plot_path logscopy_exp_train/20 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
-#python -u train.py --traindata train50.pkl --valdata val50.pkl --model_path models/copy50.pt --plot_path logscopy_exp_train/50 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
-#python -u train.py --traindata train100.pkl --valdata val100.pkl --model_path models/copy100.pt --plot_path logscopy_exp_train/100 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/new/train1.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/new/run2/val1.pkl --model_path models/copy1.pt --plot_path logscopy_exp_train/1 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/new/train2.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/new/run2/val2.pkl --model_path models/copy2.pt --plot_path logscopy_exp_train/2 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/train5.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/val5.pkl --model_path models/copy5.pt --plot_path logscopy_exp_train/5 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/train10.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/val10.pkl --model_path models/copy10.pt --plot_path logscopy_exp_train/10 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/train20.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/val20.pkl --model_path models/copy20.pt --plot_path logscopy_exp_train/20 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/train50.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/val50.pkl --model_path models/copy50.pt --plot_path logscopy_exp_train/50 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/train100.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/materials-scibert-retr-exp-train/val100.pkl --model_path models/copy100.pt --plot_path logscopy_exp_train/100 --no-generate --copy --gpu --classes 16 --num_entities 21 --num_buckets 11 --batch_size 2
 #sleep 1
 exit
