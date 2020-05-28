4fbfe966d779f685c340746969ae637ff9b02a4e
Ahsaas Bajaj <abajaj@gypsum.cm.cluster>
2020-05-28 08:45:56 -0400

    update test_k for matsci

--- copy_model/train_copy.sh
+++ copy_model/train_copy.sh
@@ -4,11 +4,11 @@
 #SBATCH --output=logscopy/copy_%j.txt  # output file
 #SBATCH -e logscopy/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-#SBATCH --partition=m40-long # Partition to submit to
-#SBATCH --mem=10000
+#SBATCH --partition=1080ti-long # Partition to submit to
+#SBATCH --mem=40000
 #
 #SBATCH --ntasks=1
 
-python -u train_finetune.py --model_path models/copy.pt --plot_path logscopy --no-generate --traindata train_ft.pkl --valdata val_ft.pkl --batch_size 1
+python -u train.py --model_path models/copy.pt --plot_path logscopy --copy --no-generate --traindata train.pkl --valdata val.pkl --batch_size 16 --gpu
 #sleep 1
 exit
--- copy_model/train_copy_exp_train.sh
+++ copy_model/train_copy_exp_train.sh
@@ -4,19 +4,18 @@
 #SBATCH --output=logscopy_exp_train/copy_%j.txt  # output file
 #SBATCH -e logscopy_exp_train/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
-#SBATCH --partition=titanx-long # Partition to submit to
+#SBATCH --partition=2080ti-long # Partition to submit to
 #SBATCH --mem=40GB
 #
 #SBATCH --ntasks=1
 
-python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-2/train1.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-2/val1.pkl --model_path models/copy1.pt --plot_path logscopy_exp_train/1 --no-generate --gpu
-python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-2/train2.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-2/val2.pkl --model_path models/copy2.pt --plot_path logscopy_exp_train/2 --no-generate --gpu
-
-# python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/train5.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/val5.pkl --model_path models/copy5.pt --plot_path logscopy_exp_train/5 --no-generate --gpu
-# python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/train10.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/val10.pkl --model_path models/copy10.pt --plot_path logscopy_exp_train/10 --no-generate --gpu
-# python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/train20.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/val20.pkl --model_path models/copy20.pt --plot_path logscopy_exp_train/20 --no-generate --gpu
-# python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/train50.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/val50.pkl --model_path models/copy50.pt --plot_path logscopy_exp_train/50 --no-generate --gpu
-# python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/train100.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/run-1/val100.pkl --model_path models/copy100.pt --plot_path logscopy_exp_train/100 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train1.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val1.pkl --model_path models/wtlabs_copy1.pt --plot_path logscopy_exp_train/1 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train2.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val2.pkl --model_path models/wtlabs_copy2.pt --plot_path logscopy_exp_train/2 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train5.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val5.pkl --model_path models/wtlabs_copy5.pt --plot_path logscopy_exp_train/5 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train10.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val10.pkl --model_path models/wtlabs_copy10.pt --plot_path logscopy_exp_train/10 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train20.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val20.pkl --model_path models/wtlabs_copy20.pt --plot_path logscopy_exp_train/20 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train50.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val50.pkl --model_path models/wtlabs_copy50.pt --plot_path logscopy_exp_train/50 --no-generate --gpu
+python -u train.py --traindata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/train100.pkl --valdata /mnt/nfs/work1/mccallum/abajaj/akbc/data/scibert-ret-exp-train/exp-k-test/new/val100.pkl --model_path models/wtlabs_copy100.pt --plot_path logscopy_exp_train/100 --no-generate --gpu
 
 #sleep 1
 exit
