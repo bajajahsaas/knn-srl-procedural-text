03417b3f3d72563bf2c828ec2c3f6a38178ee48f
Daivik Swarup <daivikswarupov@gmail.com>
2020-05-30 15:10:54 -0400

    sh scripts

--- copy_model/test_all.sh
+++ copy_model/test_all.sh
@@ -1,3 +1,12 @@
-python test_joint.py --generate --no-copy  --model_path models/generate.pt --test_output_path generate
-python test_joint.py --no-generate --copy --model_path models/copy.pt --test_output_path copy
-python test_joint.py --generate --copy  --model_path models/copy_generate.pt --test_output_path copy_generate
+#!/bin/bash
+#
+#SBATCH --job-name=TEST
+#SBATCH --output=logscopy/TEST_%j.txt  # output file
+#SBATCH -e logscopy/TEST_%j.err        # File to which STDERR will be written
+#SBATCH --gres=gpu:1
+#SBATCH --partition=1080ti-short # Partition to submit to
+#SBATCH --mem=40000
+# python test_joint.py --generate --no-copy  --model_path models/generate.pt --test_output_path generate
+#python test_k_output.py --no-generate --copy --model_path models/copy_exact.pt --test_output_path copy --valdata val_k40.pkl --gpu
+python test_k_output.py --no-generate --copy --model_path models/copy_no_label.pt --attnmethod no_label --test_output_path copy_no_label --valdata $mnt/exact/data/val_k40.pkl --gpu
+# python test_joint.py --generate --copy  --model_path models/copy_generate.pt --test_output_path copy_generate
--- copy_model/train_copy.sh
+++ copy_model/train_copy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-#SBATCH --job-name=copy
+#SBATCH --job-name=copy_no_label
 #SBATCH --output=logscopy/copy_%j.txt  # output file
 #SBATCH -e logscopy/copy_%j.err        # File to which STDERR will be written
 #SBATCH --gres=gpu:1
 #SBATCH --partition=m40-long # Partition to submit to
-#SBATCH --mem=10000
+#SBATCH --mem=50000
 #
 #SBATCH --ntasks=1
 
-python -u train_finetune.py --model_path models/copy.pt --plot_path logscopy --no-generate --traindata train_ft.pkl --valdata val_ft.pkl --batch_size 1
+python -u train.py --model_path models/copy_no_label.pt --plot_path logscopy --no-generate --copy --traindata $mnt/exact/data/train.pkl --valdata $mnt/exact/data/val.pkl --batch_size 16 --attnmethod no_label --gpu
 #sleep 1
 exit
